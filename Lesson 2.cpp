be6e7f85d34a1670cf4f04a46cc2c6570af29f34
MileX0k <81148586+MileX0k@users.noreply.github.com>
2021-11-06 21:50:36 +0500

    Update lesson 2

--- Lesson 2.cpp
+++ Lesson 2.cpp
@@ -54,6 +54,92 @@ void SortPointers(vector<T*> v1) {
 	}
 }
 
+//===========================TASK 3=============================================
+
+bool find_vowel(char vow) {            //   find
+	char res = tolower(vow);
+	string letters = "aeyuio";
+	return find(letters.begin(), letters.end(), res) != letters.end();
+}
+
+bool for_vowel(char vow) {            //   for
+	char res = tolower(vow);
+	string letters = "aeyuio";
+	int found = letters.find(res);
+	bool out;
+	found == string::npos ? out = false : out = true;
+	return out;
+}
+
+int count_if_and_find(ifstream& file) {
+
+	Timer time;
+	time.start("Count_if and Find: ");
+	int summ = 0;
+	while (!file.eof()) {
+		string raw_string = "";
+		getline(file, raw_string);
+		summ += count_if(raw_string.begin(), raw_string.end(), find_vowel);
+	}
+	time.print();
+	cout << "Vowel letters found: " << summ << endl;
+	file.close();
+	return 0;
+}
+
+int count_if_and_for(ifstream& file) {
+	
+	Timer time;
+	time.start("Count_if and For: ");
+	int summ = 0;
+	while (!file.eof()) {
+		string raw_string = "";
+		getline(file, raw_string);
+		summ += count_if(raw_string.begin(), raw_string.end(), for_vowel);
+	}
+	time.print();
+	cout << "Vowel letters found: " << summ << endl;
+	file.close();
+	return 0;
+}
+
+int for_and_find(ifstream& file){
+	
+	Timer time;
+	time.start("For and Find: ");
+	int summ = 0;
+	while (!file.eof()) {
+		string raw_string = "";
+		getline(file, raw_string);
+		for (auto i = raw_string.begin(); i < raw_string.end(); ++i)
+			if (find_vowel(*i)) ++summ;
+	}
+	time.print();
+	cout << "Vowel letters found: " << summ << endl;
+	file.close();
+	return 0;
+
+}
+
+int for_and_for(ifstream& file) {
+
+	Timer time;
+	time.start("For and For: ");
+	int summ = 0;
+	while (!file.eof()) {
+		string raw_string = "";
+		getline(file, raw_string);
+		for (auto i = raw_string.begin(); i < raw_string.end(); ++i)
+			if (for_vowel(*i)) ++summ;
+	}
+	time.print();
+	cout << "Vowel letters found: " << summ << endl;
+	file.close();
+	return 0;
+
+}
+
+
 int main()
 {
 //===========================TASK 1=============================================
@@ -86,6 +172,32 @@ int main()
 	}
 	
 	SortPointers<int>(vptr);
+
+//===========================TASK 3=============================================
+
+	cout << "------TASK 3------" << endl;
+
+	ifstream text1("War and peace.txt");
+	if (!text1.is_open()) return 0;
+	count_if_and_find(text1);
+	cout << endl;
+
+	ifstream text2("War and peace.txt");
+	if (!text2.is_open()) return 0;
+	count_if_and_for(text2);
+	cout << endl;
+
+	ifstream text3("War and peace.txt");
+	if (!text3.is_open()) return 0;
+	for_and_find(text3);
+	cout << endl;
+
+	ifstream text4("War and peace.txt");
+	if (!text4.is_open()) return 0;
+	for_and_for(text4);
+	cout << endl;
+
+
 }
 
 
